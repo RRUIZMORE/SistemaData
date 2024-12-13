fe63049f6794cac46fb92ac6a0797a69dbdf8004
Aldo Zevallos <>
2024-12-12 21:15:10 -0500

    Feat: Esto es una prueba de Commit

--- src/sistemadata/SistemaData.java
+++ src/sistemadata/SistemaData.java
@@ -16,6 +16,8 @@ public class SistemaData {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        
+        System.out.println("Esto es una prueba AZEVALLOS");
     }
     
 }
